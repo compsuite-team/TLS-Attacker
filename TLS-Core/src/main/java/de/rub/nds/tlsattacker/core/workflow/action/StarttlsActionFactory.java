631e45f89710394df8a3187c450450ad55d4e001
TillBudde <till.budde@gmx.de>
2021-01-18 20:23:22 +0100

    Added factory to summarize STARTTLS actions

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/action/StarttlsActionFactory.java
@@ -0,0 +1,85 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2020 Ruhr University Bochum, Paderborn University,
+ * and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.workflow.action;
+
+import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.connection.AliasedConnection;
+import de.rub.nds.tlsattacker.transport.ConnectionEndType;
+
+public class StarttlsActionFactory {
+
+    private StarttlsActionFactory() {
+
+    }
+
+    public static AsciiAction createIssueStarttlsCommandAction(Config tlsConfig, AliasedConnection connection,
+            ConnectionEndType sendingConnectionEnd, StarttlsMessageFactory.CommandType commandType, String encoding) {
+        StarttlsMessageFactory factory = new StarttlsMessageFactory(tlsConfig);
+        String message;
+        AsciiAction action;
+        if (connection.getLocalConnectionEndType() == sendingConnectionEnd) {
+            message = factory.createSendCommand(commandType);
+            action = new SendStarttlsCommandAction(message, encoding);
+        } else {
+            action = new ReceiveStarttlsCommandAction(encoding);
+        }
+        return action;
+    }
+
+    public static AsciiAction createStarttlsAsciiAction(Config tlsConfig, AliasedConnection connection,
+            ConnectionEndType sendingConnectionEnd, StarttlsMessageFactory.CommandType commandType, String encoding) {
+        StarttlsMessageFactory factory = new StarttlsMessageFactory(tlsConfig);
+        String message;
+        AsciiAction action;
+        if (connection.getLocalConnectionEndType() == sendingConnectionEnd) {
+            message = factory.createSendCommand(commandType);
+            action = new SendAsciiAction(message, encoding);
+        } else {
+            message = factory.createReceiveCommand(commandType);
+            action = new GenericReceiveAsciiAction(encoding);
+        }
+        return action;
+    }
+
+    public static AsciiAction createStarttlsCommunicationAction(Config tlsConfig, AliasedConnection connection,
+            ConnectionEndType sendingConnectionEnd, StarttlsMessageFactory.CommandType commandType, String encoding) {
+        StarttlsMessageFactory factory = new StarttlsMessageFactory(tlsConfig);
+        AsciiAction action;
+        String message;
+        if (connection.getLocalConnectionEndType() == sendingConnectionEnd) {
+            message = factory.createSendCommand(commandType);
+            action = new SendAsciiAction(message, encoding);
+        } else {
+            message = factory.createReceiveCommand(commandType);
+            action = new StarttlsAnswerTillAction(tlsConfig, message, encoding);
+        }
+        return action;
+    }
+
+    public static AsciiAction createServerCapabilitiesAction(Config tlsConfig, AliasedConnection connection,
+            ConnectionEndType sendingConnectionEnd, String encoding) {
+        AsciiAction action;
+        if (connection.getLocalConnectionEndType() == sendingConnectionEnd)
+            action = new SendServerCapabilitiesAction(encoding, tlsConfig);
+        else
+            action = new ReceiveServerCapabilitiesAction(encoding, tlsConfig);
+        return action;
+    }
+
+    public static AsciiAction createServerGreetingAction(Config tlsConfig, AliasedConnection connection,
+            ConnectionEndType sendingConnectionEnd, String encoding) {
+        AsciiAction action;
+        if (connection.getLocalConnectionEndType() == sendingConnectionEnd)
+            action = new SendServerGreetingAction(encoding, tlsConfig);
+        else
+            action = new ReceiveServerGreetingAction(encoding, tlsConfig);
+        return action;
+    }
+}
